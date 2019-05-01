60de86a9bda9500e02aaa5fa53aa507351a70755
mehme <mehme@LAPTOP-TB8RK01J>
2019-05-01 14:05:44 +0200

    debut de la classe joueur

--- CandyCrush/src/game/Joueur.java
+++ CandyCrush/src/game/Joueur.java
@@ -1,5 +1,29 @@
 package game;
 
 public class Joueur {
+	private String nom;
+	private int score;
+	
+	
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	public void addScore(int score) {
+		this.score = this.score + score;
+	}
+	
+	public void setScore(int score) {
+		
+		
+	}
 
 }
