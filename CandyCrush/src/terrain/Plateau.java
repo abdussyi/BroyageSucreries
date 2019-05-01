14734689304cb278ccda3fb3d09ab7b8948cecee
Abdulsamet_Akkus <abdulsamet.akkus1@etu.univ-lorraine.fr>
2019-05-01 19:52:04 +0200

    mise a jour

--- CandyCrush/src/terrain/Plateau.java
+++ CandyCrush/src/terrain/Plateau.java
@@ -154,7 +154,26 @@ public class Plateau {
 
 	
 	//coordonnée du bonbon à echanger sur le tableau en parametre
-	public void echange(int x, int y,int x1,int y2)
+	public void echange(int x, int y,int x2,int y2) throws CandyException
+	{
+		if(   (x!=x2 && y!=y2)    ||    (   (x2!=x+1||x2!=x-1)     &&     (y2!=y+1||y2!=y-1)   )   )
+		{
+			throw new CandyException("Case non conforme");
+		}
+		else if(x>taille-1||y>taille-1||x2>taille-1||y2>taille-1||x<0||y<0||x2<0||y2<0)
+		{
+			throw new IllegalArgumentException("Argument probleme case vide");
+		}
+		else
+		{
+			//c'est l'echange
+			Contenant temp = grille[x2][y2].getBonbon();
+			grille[x2][y2].setBonbon(grille[x][y].getBonbon());
+			grille[x][y].setBonbon(temp);
+		}
+	}
+	
+	public void detection(int x, int y)
 	{
 		
 	}
